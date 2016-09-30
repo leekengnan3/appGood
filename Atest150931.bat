0ada5697a7993003653ef5fb1d59a56aeaaa9a24
leekengnan <leekengnan@gmail.com>
2016-09-30 16:45:02 +0800

    for test160931

---
+++ Atest150931.bat
@@ -0,0 +1,9 @@
+set java_home=D:/java/JAVA/jdk1.8.0_92
+set ANDROID_HOME=D:/Android/sdk
+set path=D:/java/JAVA/jdk1.8.0_92/bin/;D:/java/apache-ant-1.9.4-bin/apache-ant-1.9.4/bin;%ANDROID_HOME%/sdk/platform-tools;%ANDROID_HOME%/sdk/tools;"C:/Program Files/nodejs/";C:/Users/T2941/AppData/Roaming/npm
+path=%path%;C:/Windows/system32;C:/Windows;C:/Windows/System32/Wbem;
+
+rem cordova create test160931 com.tr.app.first App160931
+rem cordova platform add android
+rem cordova build android
+rem cordova run android
